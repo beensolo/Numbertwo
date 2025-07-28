f1c1c42ed3698290fba617349ec39fa8d7cbca50
ben <ben.solo.art@gmail.com>
2025-07-27 19:48:40 -0700

    laod it bitch2

--- src/truck.js
+++ src/truck.js
@@ -6,7 +6,7 @@ const canvas = document.querySelector('#canvas');
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x202020);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 2, 25);
 
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
